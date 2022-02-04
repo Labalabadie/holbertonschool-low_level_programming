9c97b374856f0b2e1a644b163db6e142d8f76211
Agustin Labadie <4433@holbertonschool.com>
2022-02-03 16:29:44 -0800

    Tas 4 v1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
- */
+* main - This functions gives a string to standar output.
+* Return messages
+* Return: 0 success
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like buildin a multilingual puzzle");
 return (0);
 }
