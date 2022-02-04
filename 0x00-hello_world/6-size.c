ca7edd7de59c78175c87df152969f4be0975c247
Agustin Labadie <4433@holbertonschool.com>
2022-02-03 18:04:29 -0800

    Idem

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(longlong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
 	return (0);
 }
