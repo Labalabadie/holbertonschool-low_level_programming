3cecc2ae5ca188a3a51e918a822e6a38742e346b
Agustin Labadie <4433@holbertonschool.com>
2022-02-03 17:53:51 -0800

    Correct output for machines checker

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long longlong;
 	float afloat;
 
-	printf("size of a character: %lu bytes\n", sizeof(character));
-	printf("size of a interger: %lu bytes\n", sizeof(interger));
-	printf("size of a long int: %lu bytes\n", sizeof(along));
-	printf("size of a long long int: %lu bytes\n", sizeof(longlong));
-	printf("size of a float: %lu bytes\n", sizeof(afloat));
+	printf("size of a character: %lu byte(s)\n", sizeof(character));
+	printf("size of a interger: %lu byte(s)\n", sizeof(interger));
+	printf("size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("size of a long long int: %lu bytes(s)n", sizeof(longlong));
+	printf("size of a float: %lu byte(s)\n", sizeof(afloat));
 	return (0);
 }
