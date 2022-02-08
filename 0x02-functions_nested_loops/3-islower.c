cf7ea02ebc680ac2c3a3d3996a085ec45fc5513e
Agustin Labadie <4433@holbertonschool.com>
2022-02-07 17:35:30 -0800

    TAs 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
+ * c: cheking character.
  * _islower - checks for lower case characters.
+ * Return: 1 for lower case in c parameter, 0 otherwise.
  */
-int islower(int c)
+int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
 }
