910ba7b0d520f6cef12fd61c0aecaf057b469ab8
Agustin Labadie <4433@holbertonschool.com>
2022-02-08 07:55:17 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * positive_or_negative - Test if zero.
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int i;
+
+	i=98;
+	positive_or_negative(i);
+	if ( i = 0 )
+		printf("%d is positive\n", i);
+
+	return (0);
+}
