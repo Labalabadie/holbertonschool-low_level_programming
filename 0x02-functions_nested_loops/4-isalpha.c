a2f2ccedda01d23cdf1764cc0bcd3b9468b3dbc6
Agustin Labadie <4433@holbertonschool.com>
2022-02-07 18:19:14 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * c: cheking character.
+ * islower - checks for lower case characters.
+ * Return: 1 for lower case in c parameter, 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 122)
+	return (1);
+	else
+	return (0);
+	_putchar('\n');
+}
