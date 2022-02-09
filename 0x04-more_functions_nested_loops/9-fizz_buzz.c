c90638cd56c83bfb88dadcc51549a5294d362974
Agustin Labadie <4433@holbertonschool.com>
2022-02-09 13:23:40 -0800

    Betty style

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 /**
- *
+ *main - FizzBuzz test.
+ * Print numbers from 1 to 100.
+ * for every mult of 3 Fizz
+ * for every mult of 5 Buzz
+ * both FizzBuzz
+ * Return: Always 0
  */
-int main()
+int main(void)
 {
 	int a;
+
 	for (a = 1 ; a <= 100 ; a++)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (a % 3 == 0 )
+		else if (a % 3 == 0)
 		{
 			printf("Fizz");
 		}
@@ -24,5 +30,6 @@ int main()
 			printf("%d", a);
 		}
 	}
+	printf('\n');
 	return (0);
 }
