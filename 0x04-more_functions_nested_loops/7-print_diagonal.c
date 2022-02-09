8ed15bb0bcf16dfdfe0fa840f727f4832f99ab4c
Agustin Labadie <4433@holbertonschool.com>
2022-02-09 11:08:24 -0800

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_diagonal - Draw a diagonal in shell.
+ *@n: Number of lines
+ *Return: void.
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = a; b > 0; b--)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+}
