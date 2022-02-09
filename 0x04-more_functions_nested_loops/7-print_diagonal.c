ddf0c6df77ce0148045b282c3c02782ecc29a216
Agustin Labadie <4433@holbertonschool.com>
2022-02-09 11:30:47 -0800

    added if

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,17 @@ void print_diagonal(int n)
 	int a;
 	int b;
 
-	if (n > 0)
+	if (n >= 0)
 	{
-		for (a = 0; a < n; a++)
-		{
+		_putchar('\n');
+	}
+	for (a = 0; a < n; a++)
+	{
 			for (b = a; b > 0; b--)
 			{
 				_putchar(' ');
 			}
 			_putchar(92);
 			_putchar('\n');
-		}
 	}
 }
