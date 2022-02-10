2b6179b84010f14b72bfbf166b5cd372947256ce
Agustin Labadie <4433@holbertonschool.com>
2022-02-10 05:04:35 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * main - Test i set as in zero.
- * Return: 0 Success
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
