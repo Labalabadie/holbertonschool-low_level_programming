2883594fd18e1a8c141180fa111bd3d3fa15ab22
Agustin Labadie <4433@holbertonschool.com>
2022-02-10 12:49:04 -0800

    Betty style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 
- * 
+ *add -  adds two interger.
+ *@a: First interger
+ *@b: second Interger
+ * Return: Result of adding.
  */
-int add (int a, int b)
+int add(int a, int b)
 {
-	return(a + b);
+	return (a + b);
 }
