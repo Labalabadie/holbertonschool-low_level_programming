7733cb1e5cb875c2428eb05316b4b7a7ff7f7c37
Agustin Labadie <4433@holbertonschool.com>
2022-02-22 14:44:49 -0800

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_chessboard -  function that prints the chessboard.
+ *@a: pointer
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 8; j++)
+	{
+		for (i = 0; i < 8 ; i++)
+		{
+			_putchar(a[j][i]);
+		}
+		_putchar('\n');
+	}
+}
