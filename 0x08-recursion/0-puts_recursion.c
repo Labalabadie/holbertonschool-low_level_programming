4c3b63757d9815d2a0c25ceba9aefdee7d48b595
Agustin Labadie <4433@holbertonschool.com>
2022-02-23 12:45:52 -0800

    Adding caso 0 v.2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,10 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\0');
-	}
-	_putchar(*s);
-	s++;
 	if (*s != '\0')
 	{
+		_putchar(*s);
+		s++;
 		_puts_recursion(s);
 	}
 	else
