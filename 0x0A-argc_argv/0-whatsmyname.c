15bcdf36b81cc73ee073a918fca74b91739dd319
Agustin Labadie <4433@holbertonschool.com>
2022-02-25 10:54:49 -0800

    task 0 with betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ *main - Prints the name of the file.
+ *844 any unlikely number, needed for compiler.
+ *@argc: argument counter.
+ *@argv: argument value
+ *Return: 0 success.
  */
 int main(int argc, char *argv[])
 {
 	if (argc != 844)
-	printf("%s\n", argv [0]);
-	return(0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
