bceffef0ea42d80e6d3f1d02a1a0b248dfeb9b1d
Agustin Labadie <4433@holbertonschool.com>
2022-02-25 15:27:47 -0800

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - A program that adds positive numbers.
+ *@argc: Argument count. used for a counter in for loop.
+ *@argv: Argument value. Interger to be added.
+ *Return: 0 success - 1 Error.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	if (argc == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1 ; i < argc ; i++)
+	{
+		if (*argv[i] > '9' || *argv[i] < '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+
+}
