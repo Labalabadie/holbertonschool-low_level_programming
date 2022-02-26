a22720c3d359aebab65e2f811779001a77a1474a
Agustin Labadie <4433@holbertonschool.com>
2022-02-25 17:34:28 -0800

    function works, not accepts betty style

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,22 +8,46 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum;
+	int i, sum, j, k, l, m;
 
-	if (argc == 0)
+	if (argc > 2 || argc < 2)
 	{
-		printf("0\n");
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	for (i = 1 ; i < argc ; i++)
+	sum = 0;
+	i = atoi(argv[1]);
+	j = i;
+	while (i >= 25)
 	{
-		if (*argv[i] > '9' || *argv[i] < '0')
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[i]);
+		j %= 25;
+		i /= 25;
+		sum += i;
+	}
+	k = j;
+	while (j > 0 && j >= 10)
+	{
+		k %= 10;
+		j /= 10;
+		sum += j;
+	}
+	l = k;
+	while (k > 0 && k >= 5)
+	{
+		l %= 5;
+		k /= 5;
+		sum += k;
+	}
+	m = l;
+	while (l > 0 && l >= 2)
+	{
+		m %= 2;
+		l /= 2;
+		sum += l;
+	}
+	if (m > 0)
+	{
+		sum += m;
 	}
 	printf("%d\n", sum);
 	return (0);
