c3ab6fabff56da8fb2d5f25a6c5ba31f94e56538
Agustin Labadie <4433@holbertonschool.com>
2022-03-08 11:06:48 -0800

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MASCOT
+#define MASCOT
+/**
+ *struct dog - New Struct, dog, three types of data.
+ *@name: name (string)
+ *@age: age in float.
+ *@owner: owner (string)
+ *
+ *Description: Example for learning.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
