b1f902118bb590a677767d743b005ab08a933a81
Agustin Labadie <4433@holbertonschool.com>
2022-03-09 07:12:03 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ *print_name - calls a function by pointer.
+ *@name: string
+ *@f: function pointer. As argument.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	f(name);
+}
