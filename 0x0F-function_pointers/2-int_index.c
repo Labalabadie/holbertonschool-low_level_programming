1bb6b0e49198ee49d8f992f48b7f1d6d476fca36
Agustin Labadie <4433@holbertonschool.com>
2022-03-09 10:23:58 -0800

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ *int_index - function that serches for an interger.
+ *@array: given values to search in.
+ *@size:Size of array
+ *@cmp:function pointer.
+ *Return: -1 no element matches ||size <= 0.
+ *return: i. Position in the array with value != 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			break;
+	}
+	return (i);
+}
