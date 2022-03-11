c011aaf58e42e87b35aebfc9b2836d0479928c48
Agustin Labadie <4433@holbertonschool.com>
2022-03-11 11:59:17 -0800

    New conditional

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,9 +20,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (i == n - 1)
 		break;
 
-	if (separator != NULL)
+	if (i != n - 1 && separator != NULL)
 	printf("%s", separator);
 	}
-	va_end(str);
 	putchar('\n');
+	va_end(str);
 }
