6bde7e6f1112628d4d20a6af51ac6c7838b5156f
Agustin Labadie <4433@holbertonschool.com>
2022-03-11 06:26:51 -0800

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers - Prints arguments in int with separator in between.
+ *@separator: separator un string.
+ *@n: constant in interger
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list lst;
+
+	va_start(lst, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(lst, int));
+		if (i == n - 1)
+			break;
+
+		printf("%s", separator);
+	}
+	va_end(lst);
+	putchar('\n');
+}
