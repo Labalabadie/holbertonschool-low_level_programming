bd2ba369477054865cb497091b65c2c7ea44d593
Agustin Labadie <4433@holbertonschool.com>
2022-03-23 08:31:30 -0700

    task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *add_nodeint - Creates a new node at the beggining of a linked list.
+ *@head: ptr to derreferences ptr to head.
+ *@n: value n in struct.
+ *Return: address to new node.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
