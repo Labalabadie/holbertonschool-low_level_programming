5c68d28bda454c11021a4ecb277e8cd5be29fd58
Agustin Labadie <4433@holbertonschool.com>
2022-03-24 13:58:39 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ *print_binary - Prints the binary representation of a decimal.
+ *@n: Decimal number.
  */
 void print_binary(unsigned long int n)
 {
 	int a = (4 * (sizeof(n)));
 	int b = 0;
 
+	if (!n)
+		return;
 	if (n == 0)
 		putchar('0');
 	a--;
-	for (;a >= 0;a--)
+	for (; a >= 0; a--)
 	{
 		if (1 << a & n)
 		{
 		putchar('1');
 		b = 1;
-		}	
+		}
 		else if (b == 1)
 			putchar('0');
 	}
