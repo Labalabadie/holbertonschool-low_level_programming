31cc3f417dad82672825f3ce8931f59fe8891d32
Agustin Labadie <4433@holbertonschool.com>
2022-03-24 11:16:43 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ *  *_pow - Does exponential arithmetic.
+ *   *@a: Base number.
+ *    *@b: exponential number.
+ *     *Return: Result.
+ *      */
+int _pow(int a, int b)
+{
+	int result = 1;
+
+	if (b < 1)
+		return (1);
+	for (; b > 0; b--)
+	{
+		result = result * a;
+	}
+	return (result);
+}
+/**
+ *
+ *
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	int j = 0;
+	unsigned int i = 0;
+
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+		return (0);
+	}
+	a--;
+	while (a >= 0)
+	{
+		if (b[a] == '1')
+		{
+			i += _pow(2, j);
+		}
+		a--;
+		j++;
+	}
+	return (i);
+}
