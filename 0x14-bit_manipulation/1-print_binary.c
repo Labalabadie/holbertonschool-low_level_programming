ee50e58bb4308b428f608c854c134450a516b847
Agustin Labadie <4433@holbertonschool.com>
2022-03-24 14:04:29 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,19 +8,17 @@ void print_binary(unsigned long int n)
 	int a = (4 * (sizeof(n)));
 	int b = 0;
 
-	if (!n)
-		return;
 	if (n == 0)
-		putchar('0');
+		_putchar('0');
 	a--;
 	for (; a >= 0; a--)
 	{
 		if (1 << a & n)
 		{
-		putchar('1');
+		_putchar('1');
 		b = 1;
 		}
 		else if (b == 1)
-			putchar('0');
+			_putchar('0');
 	}
 }
