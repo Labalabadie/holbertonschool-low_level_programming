2eac26150dc8f4e1e6b98f94faed5847f9f0430c
Agustin Labadie <4433@holbertonschool.com>
2022-03-29 12:11:52 -0700

    new NULL restriction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int ab;
 	int bufleng;
 
-	if (!text_content)
+	if (text_content == NULL)
 		text_content = '\0';
 	for (bufleng = 0; text_content[bufleng] != '\0'; bufleng++)
 	{
