9ca5571ab13b6351b538286a04297e036f71631e
Agustin Labadie <4433@holbertonschool.com>
2022-03-29 11:57:37 -0700

    added trun

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int bufleng;
 
 	if (!text_content)
-		text_content = '\0';
+		text_content = "";
 
 	for (bufleng = 0; text_content[bufleng] != '\0'; bufleng++)
 	{
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return(-1);
 
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return(-1);
 	ab = write(fd, text_content, sizeof(char)* bufleng);
