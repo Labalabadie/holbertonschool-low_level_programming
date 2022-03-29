4b2be0cf20d198c723b3fb3c7277387a97a47249
Agustin Labadie <4433@holbertonschool.com>
2022-03-29 08:21:50 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *read_textfile - A func that reads a txt file and prints it to POSIX stdout.
+ *@filename: Name of the file in argv.
+ *@letters: Number of char to be printed.
+ *return: 0 if requirement not encountered, else nro of char printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int ab;
+	int fd;
+	char *buf;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0 || filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	ab = read(fd, buf, letters);
+	write(STDOUT_FILENO, buf, ab);
+	close(fd);
+	free(buf);
+	if (ab < 0)
+		return(0);
+	return(ab);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef POSIX
+#define POSIX
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
