45dd336e82c721c413c281547671b1778d092799
Agustin Labadie <4433@holbertonschool.com>
2022-03-29 11:45:35 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,11 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	int ab;
 	int bufleng;
+
+	if (! text_content)
+		text_content = '\0';
 
 	for (bufleng = 0; text_content[bufleng] != '\0'; bufleng++)
 	{
@@ -19,7 +23,11 @@ int create_file(const char *filename, char *text_content)
 		return(-1);
 
 	fd = open(filename, O_CREAT | O_RDWR, 0600);
-	write(fd, text_content, sizeof(char)* bufleng);
+	if (fd < 0)
+		return(-1);
+	ab = write(fd, text_content, sizeof(char)* bufleng);
+	if (ab < 0)
+		return(-1);
 	close(fd);
 	return(1);
 }
