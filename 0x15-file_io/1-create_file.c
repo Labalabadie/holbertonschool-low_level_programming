5c5562d3081e45842c65e4c7c788797133fea1ba
Agustin Labadie <4433@holbertonschool.com>
2022-03-29 11:46:51 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int bufleng;
 
 	if (! text_content)
-		text_content = '\0';
+		*text_content = '\0';
 
 	for (bufleng = 0; text_content[bufleng] != '\0'; bufleng++)
 	{
