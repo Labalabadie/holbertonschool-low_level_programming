65a6b94c2c8b91d6c08324b285466cd164c2414c
Agustin Labadie <4433@holbertonschool.com>
2022-03-29 12:09:32 -0700

    Betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,6 +11,8 @@ int create_file(const char *filename, char *text_content)
 	int ab;
 	int bufleng;
 
+	if (!text_content)
+		text_content = '\0';
 	for (bufleng = 0; text_content[bufleng] != '\0'; bufleng++)
 	{
 	}
