a9a1d179e40d12d6b59f4861034ef334e0d9f611
Agustin Labadie <4433@holbertonschool.com>
2022-05-05 14:36:33 -0700

    task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- *
- *
- *
+ *sum_dlistint -  sum of all the data (n) of a dlistint_t linked list.
+ *@head: pointer to first node.
+ *Return: sum.
  */
 int sum_dlistint(dlistint_t *head)
 {
