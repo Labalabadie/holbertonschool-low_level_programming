f61bef429b73874de03f7dd92ccd094c509268ff
Agustin Labadie <4433@holbertonschool.com>
2022-05-06 09:49:41 -0700

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current, *new;
 
 	if (!h || !*h)
-		return (NULL);
+
+		return (add_dnodeint_end(h, n));
 
 	current = *h;
 
