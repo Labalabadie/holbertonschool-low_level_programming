63787f32c173ad3c461ff66a9ba735efb056f7cc
Agustin Labadie <4433@holbertonschool.com>
2022-05-27 14:55:34 -0700

    git push

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,21 +1,28 @@
 #include "hash_tables.h"
 
 /**
- *
- *
- *
+ *hash_table_create - Function that creates a has table
+ *@size: size of the array.
+ *Return: Pointer to new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t ht;
+	hash_table_t *ht;
 
 	if (size == 0)
 		return (NULL);
 
-	ht = malloc(sizeof((hast_tables_t*) * size));
+	ht = malloc(sizeof(hash_table_t));
 
 	if (!ht)
 		return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
 	return (ht);
 }
