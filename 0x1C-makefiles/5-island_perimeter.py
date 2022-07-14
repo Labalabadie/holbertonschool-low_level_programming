de5b6dadd364fa3ecc31631a9f85efaa146f6d04
Agustin Labadie <4433@holbertonstudents.com>
2022-07-14 16:29:04 -0700

    5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """This is a function that calculates perimeter on an island a virtual map"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid [i] [j] == 1:
+                if grid [i -1] [j] == 0 or i == 0:
+                    perimeter += 1
+                if grid [i +1] [j] == 0 or not grid[i + 1][j]:
+                    perimeter += 1
+                if grid [i][j -1] == 0 or j == 0:
+                    perimeter += 1
+                if grid [i][j +1] == 0 or not grid[i][j + 1]:
+                    perimeter += 1
+    return perimeter
