f2e5c33cab3bf405968e4e42c4efc347f65b128e
Agustin Labadie <4433@holbertonstudents.com>
2022-07-14 16:56:21 -0700

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,12 +8,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid)):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0 or i == 0:
+                if grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i + 1][j] == 0 or not grid[i + 1][j]:
+                if grid[i + 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0 or j == 0:
+                if grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j + 1] == 0 or not grid[i][j + 1]:
+                if grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
