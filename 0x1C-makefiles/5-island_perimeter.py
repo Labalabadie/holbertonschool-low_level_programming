b9c29f8e858e1520c11efcd176c2c63d19a54ab6
Agustin Labadie <4433@holbertonstudents.com>
2022-07-14 17:20:29 -0700

    new ranges below

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,17 +5,19 @@
 def island_perimeter(grid):
     """This is a function that calculates per on an island a virtual map"""
     perimeter = 0
-    i = 0
-    j = 0
     for i in range(len(grid)):
         for j in range(len(grid)):
             if grid[i][j] == 1:
-                if i <= 0 or not grid[i - 1][j]:
+                if i == 0:
                     perimeter += 1
-                if j <= 0 or not grid[i][j - 1]:
+                elif grid[i - 1][j] == 0:
                     perimeter += 1
-                if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+                if grid[i + 1][j] == 0 or not grid[i + 1][j]:
                     perimeter += 1
-                if i >= len(grid) - 1 or not grid[i + 1][j]:
+                if j == 0:
+                    perimeter += 1
+                elif grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0 or not grid[i][j + 1]:
                     perimeter += 1
     return perimeter
