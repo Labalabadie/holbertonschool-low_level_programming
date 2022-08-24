ddff2a1364b67423e8d681cb291540302e19904f
labalabadie <labalabadie@gmail.com>
2022-08-24 08:07:02 -0700

    0 completed

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searchs for a value in an array using, linear search.
+ * @array: pointer to array.
+ * @size: size of array.
+ * @value: value to compare and find.
+ * Return: position of array.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	for (i = 0; i < size ; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
